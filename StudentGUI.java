4b3353ba1de9f9910f2c28f7d94a7b4a7ceb7dcd
Ankit kumar bindu <ank.kumar077@gmail.com>
2023-08-10 13:24:28 +0545

    update

--- StudentGUI.java
+++ StudentGUI.java
@@ -1128,7 +1128,7 @@ public class StudentGUI implements ActionListener{
 
                                 studentFound = true;
                                 if(regularStudent.getIsGrantedScholarship()){
-                                    JOptionPane.showMessageDialog(newFrame,"The Student " + regularStudent.getStudentName()+ " has granted Scolarship."+"Graduated from" +regularStudent.getCourseName() + " with Enrollment ID:" +regularStudent.getEnrollmentID() + " and Date Of Enrollment: " + regularStudent.getDateOfEnrollment(), "Certificate  Granted", JOptionPane.INFORMATION_MESSAGE);
+                                    JOptionPane.showMessageDialog(newFrame,"The Student " + regularStudent.getStudentName()+ " has granted Scolarship."+" Graduated from" +regularStudent.getCourseName() + " with Enrollment ID:" +regularStudent.getEnrollmentID() + " and Date Of Enrollment: " + regularStudent.getDateOfEnrollment(), "Certificate  Granted", JOptionPane.INFORMATION_MESSAGE);
                                  
                                     } else{
                                     JOptionPane.showMessageDialog(newFrame, "The Student " + regularStudent.getStudentName()+" has Graduated from" +regularStudent.getCourseName() + " with Enrollment ID:" +regularStudent.getEnrollmentID() + " and Date Of Enrollment: " + regularStudent.getDateOfEnrollment(), "Certificate not Granted", JOptionPane.INFORMATION_MESSAGE);
